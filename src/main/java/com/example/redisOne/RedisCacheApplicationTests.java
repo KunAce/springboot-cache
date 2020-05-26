150166b99890c2a661d0b8faca631d5e9660a359
KunAce <wxkace@gmail.com>
2020-05-25 18:38:12 -0700

    Added cache support by using stand-alone Redis server

---
+++ src/main/java/com/example/redisOne/RedisCacheApplicationTests.java
@@ -0,0 +1,31 @@
+package com.example.redisOne;
+
+import com.example.redisOne.Book;
+import com.example.redisOne.BookDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisCacheApplicationTests {
+    @Autowired
+    BookDao bookDao;
+    @Test
+    public void contextLoads() {
+        bookDao.getBookById(1);
+        bookDao.getBookById(1);
+        bookDao.deleteBookById(1);
+        Book b3 = bookDao.getBookById(1);
+        System.out.println("b3:" + b3);
+       Book b = new Book();
+        b.setName("SQLBook");
+        b.setAuthor("SQLauthor");
+        b.setId(1);
+        bookDao.updateBookById(b);
+        Book b4 = bookDao.getBookById(1);
+        System.out.println("b4:" + b4);
+    }
+}
